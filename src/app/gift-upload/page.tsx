42d22d5ec6a6a8ff134e74c0100daaba5b482c17
xxxjinn <tlsguswls319@naver.com>
2025-02-04 02:44:00 +0900

    design: 선물 정보 업로드 페이지 기본 구조 잡기

--- src/app/gift-upload/page.tsx
+++ src/app/gift-upload/page.tsx
@@ -1,7 +1,18 @@
+import CharacterCountInput from "@/components/common/CharacterCountInput";
+import InputLink from "@/components/gift-upload/InputLink";
+import InputReason from "@/components/gift-upload/InputReason";
+import { Button } from "@/components/ui/button";
+
 const page = () => {
   return (
-    <div>
-      <p>선물 채우기 페이지</p>
+    <div className="flex flex-col p-4 gap-[50px]">
+      <div className="flex flex-col gap-6 bg-pink-300">
+        <div className="h-[88px] bg-pink-100">임시</div>
+        <CharacterCountInput maxLength={20} placeholder="선물명을 적어주세요" />
+      </div>
+      <InputReason />
+      <InputLink />
+      <Button size="lg">채우기 완료</Button>
     </div>
   );
 };
---
+++ src/components/gift-upload/InputReason.tsx
@@ -0,0 +1,10 @@
+const InputReason = () => {
+  return (
+    <div className="flex flex-col gap-4 h-[240px] bg-pink-100">
+      <p>이 선물을 고른 이유를 적어 함께 전달해볼까요?</p>
+      <div className="h-[208px] bg-pink-200">인풋공간</div>
+    </div>
+  );
+};
+
+export default InputReason;
