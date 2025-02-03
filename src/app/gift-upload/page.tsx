e259192460d8ff6fb38c11a19eeec76e80401e31
xxxjinn <tlsguswls319@naver.com>
2025-02-04 03:38:34 +0900

    feat: 선물 상세 업로드 - 이미지 업로드 컴포넌트 구현

--- src/app/gift-upload/page.tsx
+++ src/app/gift-upload/page.tsx
@@ -1,13 +1,21 @@
 import CharacterCountInput from "@/components/common/CharacterCountInput";
 import InputLink from "@/components/gift-upload/InputLink";
 import InputReason from "@/components/gift-upload/InputReason";
+import UploadImageList from "@/components/gift-upload/UploadImageList";
 import { Button } from "@/components/ui/button";
 
 const page = () => {
   return (
     <div className="flex flex-col p-4 gap-[50px]">
-      <div className="flex flex-col gap-6 bg-pink-300">
-        <div className="h-[88px] bg-pink-100">임시</div>
+      <div className="flex flex-col gap-1">
+        <div
+          className="w-full overflow-x-auto h-[110px] flex items-center"
+          style={{ scrollbarWidth: "none" }}
+        >
+          <div className="min-w-max">
+            <UploadImageList />
+          </div>
+        </div>
         <CharacterCountInput maxLength={20} placeholder="선물명을 적어주세요" />
       </div>
       <InputReason />
