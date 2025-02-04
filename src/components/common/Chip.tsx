3daee02624fd198c7367740e135fc5c63475b05e
xxxjinn <tlsguswls319@naver.com>
2025-02-04 14:42:33 +0900

    design: Chip 스타일 수정

--- src/components/common/Chip.tsx
+++ src/components/common/Chip.tsx
@@ -9,9 +9,9 @@ const Chip = ({ text, isActive, onClick }: ChipProps) => {
     <span
       onClick={onClick}
       className={`inline-flex items-center justify-center min-w-[55px] h-[28px] px-3 rounded-[6px] border-[1.4px] cursor-pointer 
-        ${isActive ? "bg-pink-400 border-pink-500 text-white" : "bg-white hover:bg-pink-400 hover:border-pink-500 hover:text-white"}`}
+        ${isActive ? "bg-pink-500 border-pink-500 text-white" : "bg-white hover:bg-pink-500 hover:border-pink-500 hover:text-white"}`}
     >
-      <p className="m-0 text-xs">{text}</p>
+      <p className="m-0 text-xs font-medium">{text}</p>
     </span>
   );
 };
