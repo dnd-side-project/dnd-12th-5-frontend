bfe0f62ec9dcc8476e9ea234de860399e67b8a16
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2025-02-11 20:44:18 +0900

    design: 설정 페이지 - 연결된 계정 텍스트/로그아웃 텍스트 클릭 안되도록 - '카카오 >' 버튼에 cursor: pointer 속성 추가

--- src/app/setting/page.tsx
+++ src/app/setting/page.tsx
@@ -7,11 +7,13 @@ const page = () => {
   return (
     <div className="px-4">
       <div className="text-[15px] py-[18px] border-b-[1px] border-[#f4f4f4] flex justify-between items-center">
-        <Link href={"/setting/account"}>연결된 계정</Link>
-        <div className="flex justify-center items-center">
-          <p>카카오</p>
-          <Image src={ArrowIcon} alt="arrow" width={14} height={14} />
-        </div>
+        <p>연결된 계정</p>
+        <Link href={"/setting/account"}>
+          <div className="flex justify-center items-center cursor-pointer">
+            <p>카카오</p>
+            <Image src={ArrowIcon} alt="arrow" width={14} height={14} />
+          </div>
+        </Link>
       </div>
       <div className="text-[15px] py-[18px]  border-b-[1px] border-[#f4f4f4]">
         <Link href={"/setting/notice"}>공지사항</Link>
@@ -20,7 +22,7 @@ const page = () => {
         <Link href={"/setting/version"}>버전 정보</Link>
       </div>
       <div className="text-[15px] py-[18px] border-b-[1px] border-[#f4f4f4] text-symantic-negative">
-        <Link href={"/"}>로그아웃</Link>
+        <p>로그아웃</p>
       </div>
     </div>
   );
