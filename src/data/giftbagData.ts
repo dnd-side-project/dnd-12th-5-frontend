83cf412d1ccd7bfe0a1812635b7d4903409df1d9
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2025-02-15 18:48:16 +0900

    data: 내가 만든 보따리 임시 데이터 추가

---
+++ src/data/giftbagData.ts
@@ -0,0 +1,70 @@
+// 임시 데이터
+
+import { GiftBag } from "@/types/giftbag/types";
+
+export const giftBagData: GiftBag[] = [
+  {
+    id: 0,
+    name: "보따리 1",
+    design_type: "/img/giftBag_red.svg",
+    isRead: true,
+    status: "PUBLISHED",
+    created_at: "2025-02-07",
+  },
+  {
+    id: 1,
+    name: "보따리 2",
+    design_type: "/img/giftBag_pink.svg",
+    isRead: true,
+    status: "COMPLETED",
+    created_at: "2025-02-03",
+  },
+  {
+    id: 2,
+    name: "보따리 3",
+    design_type: "/img/giftBag_blue.svg",
+    isRead: true,
+    status: "DRAFT",
+    created_at: "2025-01-11",
+  },
+  {
+    id: 3,
+    name: "보따리 4",
+    design_type: "/img/giftBag_yellow.svg",
+    isRead: false,
+    status: "COMPLETED",
+    created_at: "2025-01-05",
+  },
+  {
+    id: 4,
+    name: "보따리 5",
+    design_type: "/img/giftBag_red.svg",
+    isRead: true,
+    status: "PUBLISHED",
+    created_at: "2025-02-07",
+  },
+  {
+    id: 5,
+    name: "보따리 6",
+    design_type: "/img/giftBag_pink.svg",
+    isRead: true,
+    status: "COMPLETED",
+    created_at: "2025-02-03",
+  },
+  {
+    id: 6,
+    name: "보따리 7",
+    design_type: "/img/giftBag_blue.svg",
+    isRead: true,
+    status: "DRAFT",
+    created_at: "2025-01-11",
+  },
+  {
+    id: 7,
+    name: "보따리 8",
+    design_type: "/img/giftBag_yellow.svg",
+    isRead: false,
+    status: "COMPLETED",
+    created_at: "2025-01-05",
+  },
+];
