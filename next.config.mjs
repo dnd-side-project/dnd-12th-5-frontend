df9824b6ccc8347ad39df77f92e6d5a1b5da2653
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2025-02-15 15:29:49 +0900

    fix: process.env.NEXT_PUBLIC_API_URL 빌드 에러, 절대 경로로 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async rewrites() {
-     const apiUrl =
-       process.env.NEXT_PUBLIC_API_URL;
     return [
       {
         source: "/api/v1/:path*",
-        destination: `${apiUrl}/api/v1/:path*`,
+        destination: 'https://api.picktory.net/api/v1/:path*',
       },
     ];
   },
