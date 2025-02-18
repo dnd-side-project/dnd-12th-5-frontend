c494bd275f883f1a72d335c210082cf7465e040b
xxxjinn <tlsguswls319@naver.com>
2025-02-18 19:35:59 +0900

    design: 상대방이 받아보는 페이지 step3 - 디자인 수정

--- src/app/giftbag/[id]/step3.tsx
+++ src/app/giftbag/[id]/step3.tsx
@@ -17,20 +17,26 @@ const Step3 = () => {
   };
 
   return (
-    <div>
-      <div className="mt-[131px] mb-[133px] flex flex-col justify-center items-center gap-8">
-        <div className="w-[200px] h-[200px] flex justify-center items-center">
+    <div className="relative w-full overflow-hidden h-full">
+      <div
+        className="absolute top-0 left-0 w-full h-full bg-cover bg-center"
+        style={{ backgroundImage: "url('/img/background_union.svg')" }}
+      />
+      <div className="relative z-10 flex flex-col gap-[38px] justify-center items-center min-h-screen">
+        <div className="w-[230px] h-[230px] flex justify-center items-center">
           <Image src={Delivery} alt="deliveryMan" />
         </div>
-        <p className="text-lg font-bold font-nanum">
-          정성껏 답변을 남겨주셔서 감사해요!
-        </p>
-        <p className="text-center text-sm font-nanum text-gray-700 pt-[2px]">
-          저는 답변을 전달하러 가볼게요! <br /> 바쁘다 바빠~
-        </p>
+        <div className="flex flex-col gap-[13px]">
+          <p className="text-lg font-bold font-nanum">
+            정성껏 답변을 남겨주셔서 감사해요!
+          </p>
+          <p className="text-center text-sm font-nanum text-gray-700 pt-[2px]">
+            저는 답변을 전달하러 가볼게요! <br /> 바쁘다 바빠~
+          </p>
+        </div>
         <Button
-          variant="link"
-          className="text-xs text-gray-400 underline"
+          variant="secondary"
+          className="text-xs text-gray-400 w-[212px] rounded-[60px] bg-white"
           onClick={handleGoBack}
         >
           내가 받은 선물 다시보러 가기
