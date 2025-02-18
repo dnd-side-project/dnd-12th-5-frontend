cddb37b9afeb2671579d076013cc624946b9b104
xxxjinn <tlsguswls319@naver.com>
2025-02-19 02:49:28 +0900

    chore: next/image 태그 사용 시 s3 이미지 사용 허용

--- next.config.mjs
+++ next.config.mjs
@@ -4,9 +4,14 @@ const nextConfig = {
     return [
       {
         source: "/api/v1/:path*",
-        destination: 'https://api.picktory.net/api/v1/:path*',
+        destination: "https://api.picktory.net/api/v1/:path*",
       },
     ];
+  },
+  images: {
+    domains: [
+      "dnd-picktory-gift-bundles-bucket.s3.ap-northeast-2.amazonaws.com",
+    ],
   },
 };
 
