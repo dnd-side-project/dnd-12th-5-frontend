0b183992fac040210e98f9e9b23842b37a94af4b
xxxjinn <tlsguswls319@naver.com>
2025-02-18 21:48:40 +0900

    refactor: 배달부 관련 매핑 정리

--- src/app/giftbag/[id]/step1.tsx
+++ src/app/giftbag/[id]/step1.tsx
@@ -1,7 +1,11 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { CHARACTER_MAP } from "@/data/deliveryCharacterData";
+import {
+  CHARACTER_EN_MAP,
+  CHARACTER_MAP,
+  DELIVERY_RECEIVE_TEXT_MAP,
+} from "@/data/deliveryCharacterData";
 import Image from "next/image";
 import { useParams, useRouter } from "next/navigation";
 
@@ -10,25 +14,17 @@ interface Step1Props {
   color: string;
 }
 
-{
-  /* 임시 매핑, 추후 백엔드 답변에 따라 수정해야 할 수도 있음 */
-}
-
-const TEXT_MAP: Record<string, string> = {
-  pori: "똑똑 서프라이즈~ @ 보따리가 도착했어요!",
-  chichi: "정성이 가득 담긴 @ 선물 보따리를 가져왔어요..",
-  max: "어.. 오다 주웠다! @ 궁금하면 한번 열어보던가!",
-  hearty: "두근두근! @사랑이 담긴 보따리를 같이 열어볼까요?",
-};
-
 const Step1 = ({ delivery, color }: Step1Props) => {
   const router = useRouter();
   const { id } = useParams() as { id: string };
 
-  const character = CHARACTER_MAP[delivery] || "pori";
-  const displayText = TEXT_MAP[character] || TEXT_MAP["pori"];
+  const character = CHARACTER_MAP[delivery] || "포리";
+  const displayText =
+    DELIVERY_RECEIVE_TEXT_MAP[character] || DELIVERY_RECEIVE_TEXT_MAP["포리"];
 
-  const imageSrc = `/img/${character}_${color}.svg`;
+  // 한글 -> 영어로 변환 후 이미지 경로 설정
+  const characterEn = CHARACTER_EN_MAP[character] || "pori";
+  const imageSrc = `/img/${characterEn}_${color}.svg`;
 
   const handleOnClick = () => {
     // api 추가
@@ -39,10 +35,10 @@ const Step1 = ({ delivery, color }: Step1Props) => {
     <div className="h-full flex flex-col gap-[90px] justify-center items-center relative">
       <div className="h-[calc(100%-68px)] flex flex-col justify-center items-center gap-8">
         <p className="text-lg font-bold font-nanum text-center">
-          {displayText.split("@").map((line, index) => (
+          {displayText.split("\n").map((line, index) => (
             <span key={index}>
               {line}
-              {index !== displayText.split("@").length - 1 && <br />}
+              {index !== displayText.split("\n").length - 1 && <br />}
             </span>
           ))}
         </p>
