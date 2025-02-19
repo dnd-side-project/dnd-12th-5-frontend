c58543e85930bf3383f3efb2bc6db33f354dd136
xxxjinn <tlsguswls319@naver.com>
2025-02-19 16:02:14 +0900

    feat: 보따리 생성 api 연결 (수정 필요)

---
+++ src/api/giftbag/api.ts
@@ -0,0 +1,43 @@
+import { GIFTBAG_COLORS } from "@/constants/constants";
+import { GiftBox } from "@/types/giftbag/types";
+
+{
+  /** 보따리 생성 api */
+}
+export const createGiftBag = async ({
+  giftBagName,
+  selectedBagIndex,
+  giftBoxes,
+}: {
+  giftBagName: string;
+  selectedBagIndex: number;
+  giftBoxes: GiftBox[];
+}) => {
+  const response = await fetch("/api/v1/bundles", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+    },
+    body: JSON.stringify({
+      name: giftBagName,
+      designType: GIFTBAG_COLORS[selectedBagIndex] || "RED",
+      gifts: giftBoxes
+        .filter((gift) => gift.name.trim() !== "")
+        .map((gift) => ({
+          name: gift.name,
+          message: gift.reason, //수정 필요
+          purchaseUrl: gift.purchase_url,
+          imageUrls: gift.imageUrls?.length
+            ? gift.imageUrls
+            : ["https://www.naver.com"], //수정 필요
+        })),
+    }),
+  });
+
+  if (!response.ok) {
+    throw new Error("보따리 만들기 실패");
+  }
+
+  return response.json();
+};
