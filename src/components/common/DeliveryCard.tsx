30f350b86ece11399ba138c4ea50797de7d4a9bd
xxxjinn <tlsguswls319@naver.com>
2025-02-19 16:10:58 +0900

    design: 배달부카드 padding 수정

--- src/components/common/DeliveryCard.tsx
+++ src/components/common/DeliveryCard.tsx
@@ -16,7 +16,7 @@ const DeliveryCard = ({
   return (
     <div
       onClick={onClick}
-      className="flex flex-col items-center justify-center gap-[12px] px-[14px] py-[10px] box-border bg-gray-50 rounded-[17px] cursor-pointer border-[1.4px] border-gray-100 hover:opacity-70 w-[162px] h-[212px]"
+      className="flex flex-col items-center justify-center gap-[12px] px-[14px] pb-[10px] pt-[28px] box-border bg-gray-50 rounded-[17px] cursor-pointer border-[1.4px] border-gray-100 hover:opacity-70 w-[162px] h-[212px]"
     >
       <Image src={imageSrc} alt="delivery" width={120} height={120} />
       <div className="text-center text-gray-800">
