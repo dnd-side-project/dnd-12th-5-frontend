be7cb782a146ebabfbdcd35b61641691b3efc5f7
xxxjinn <tlsguswls319@naver.com>
2025-03-26 17:49:50 +0900

    style: input 요소 shadow 없앰

--- src/components/common/CharacterCountInput.tsx
+++ src/components/common/CharacterCountInput.tsx
@@ -34,7 +34,7 @@ const CharacterCountInput = ({
         onChange={handleChange}
         maxLength={maxLength}
         placeholder={placeholder}
-        className="border-[#F3F4F8]"
+        className="border-[#F3F4F8] shadow-none"
       />
     </div>
   );
