9893545b9c93337b89dcd001ae3ad1bcf49e0824
xxxjinn <tlsguswls319@naver.com>
2025-04-29 21:59:02 +0900

    feat: 선물상자 상태 업데이트 util 함수 구현

--- src/utils/giftBoxUtils.ts
+++ src/utils/giftBoxUtils.ts
@@ -1,6 +1,7 @@
 import { useGiftStore } from "@/stores/gift-upload/useStore";
+import { GiftBox } from "@/types/bundle/types";
 
-/** 선물 상자 초기화 */
+/** 선물상자 초기화 */
 export const resetGiftBoxes = () => {
   useGiftStore.setState({
     giftBoxes: Array(6).fill({
@@ -13,5 +14,12 @@ export const resetGiftBoxes = () => {
       imgUrls: [],
       id: null,
     }),
+  });
+};
+
+/** 선물상자 상태 업데이트 */
+export const updateGiftBoxesFromResponse = (gifts: GiftBox[]) => {
+  gifts.forEach((gift, index) => {
+    useGiftStore.getState().updateGiftBox(index, { id: gift.id });
   });
 };
