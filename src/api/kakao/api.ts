e1591fed328bacc929cf3e1cb757549fcdac3cf4
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2025-04-29 21:01:25 +0900

    fix: 카카오 로그인 요청 interceptor 영향 없도록 axios 직접 사용

--- src/api/kakao/api.ts
+++ src/api/kakao/api.ts
@@ -1,12 +1,22 @@
+import axios from "axios";
+
 import { PICKTORY_API } from "../api-url";
-import axiosInstance from "../axiosInstance";
 import { handleAxiosError } from "@/utils/axios";
 
 export const kakaoLogin = async (code: string) => {
   try {
-    const response = await axiosInstance.post(PICKTORY_API.login, {
-      code: code,
-    });
+    const response = await axios.post(
+      PICKTORY_API.login,
+      {
+        code,
+      },
+      {
+        baseURL: `${process.env.NEXT_PUBLIC_API_BASE_PATH}`,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+    );
 
     return response.data;
   } catch (error) {
