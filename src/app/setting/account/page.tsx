175a669d2b2632476c39a0416dc4785dab692573
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2025-05-04 17:42:26 +0900

    feat: 회원 탈퇴 drawer에 close 버튼 추가 및 간격 수정

--- src/app/setting/account/page.tsx
+++ src/app/setting/account/page.tsx
@@ -14,6 +14,7 @@ import {
 } from "@/components/ui/drawer";
 import { Button } from "@/components/ui/button";
 import { useDeleteUserMutation } from "@/queries/useDeleteUserMutation";
+import CloseIcon from "/public/icons/close.svg";
 
 const Page = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -35,10 +36,15 @@ const Page = () => {
         </DrawerTrigger>
 
         <DrawerContent>
-          <DrawerHeader className="relative mt-3 flex justify-center py-3">
-            <DrawerTitle>회원 탈퇴</DrawerTitle>
+          <DrawerHeader className="relative flex justify-center py-3">
+            <DrawerTitle>
+              회원 탈퇴
+              <DrawerClose className="absolute right-4 top-3">
+                <Icon src={CloseIcon} alt="close" size="large" />
+              </DrawerClose>
+            </DrawerTitle>
           </DrawerHeader>
-          <p className="text-center text-sm text-gray-500">
+          <p className="mb-[22px] mt-[26px] text-center text-sm text-gray-500">
             지금까지 만든 보따리가 모두 사라져요. <br />
             그래도 탈퇴하시겠어요?
           </p>
