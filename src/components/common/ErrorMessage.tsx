52148b9c8cb556751e500343889535a2e5e5a4de
xxxjinn <tlsguswls319@naver.com>
2025-05-08 16:35:17 +0900

    style: 에러메세지 컴포넌트 스타일 수정

--- src/components/common/ErrorMessage.tsx
+++ src/components/common/ErrorMessage.tsx
@@ -1,5 +1,7 @@
 const ErrorMessage = ({ message }: { message: string }) => {
-  return <p className="text-xs text-symantic-negative">{message}</p>;
+  return (
+    <p className="text-xs font-medium text-symantic-negative">{message}</p>
+  );
 };
 
 export default ErrorMessage;
