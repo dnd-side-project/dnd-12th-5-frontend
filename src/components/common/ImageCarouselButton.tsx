052af5162f923541a6c7eecea8b2509b50ddafba
xxxjinn <tlsguswls319@naver.com>
2025-05-09 19:18:27 +0900

    refactor: onClick 삭제

--- src/components/common/ImageCarouselButton.tsx
+++ src/components/common/ImageCarouselButton.tsx
@@ -2,12 +2,9 @@ import { Icon } from "./Icon";
 
 import ImageIcon from "/public/icons/image_carousel.svg";
 
-const ImageCarouselButton = ({ onClick }: { onClick: () => void }) => {
+const ImageCarouselButton = () => {
   return (
-    <button
-      onClick={onClick}
-      className="flex h-8 w-8 cursor-pointer items-center justify-center rounded-full bg-white shadow"
-    >
+    <button className="flex h-8 w-8 cursor-pointer items-center justify-center rounded-full bg-white shadow">
       <Icon src={ImageIcon} />
     </button>
   );
