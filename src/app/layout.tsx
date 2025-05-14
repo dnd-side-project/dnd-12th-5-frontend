467b84a56f5500fc2d4e679e57cddd49fe5d067f
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2025-05-14 18:00:52 +0900

    refactor: font weight 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,13 +16,13 @@ import type { Metadata } from "next";
 const pretendard = localFont({
   src: "./fonts/PretendardVariable.woff2",
   variable: "--font-pretendard",
-  weight: "100 200 300 400 500 600 700 800 900",
+  weight: "400 500 600 700",
 });
 
 const nanumSquareRound = localFont({
   src: "./fonts/NanumSquareRound.ttf",
   variable: "--font-nanum-square-round",
-  weight: "100 200 300 400 500 600 700 800 900",
+  weight: "400 500 600 700",
 });
 
 export const metadata: Metadata = {
